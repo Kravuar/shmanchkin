828ec7db35955262c754cab1e52445bacbb9cd14
kemper0110 <danil2003.2043@gmail.com>
2023-11-02 16:50:40 +0300

    + twin.macro - swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,43 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
   resolve: {
     alias: {
       "@": "/frontend",
       "assets": "/frontend/assets",
     }
   },
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        'babel-plugin-macros',
+        [
+          '@emotion/babel-plugin-jsx-pragmatic',
+          {
+            export: 'jsx',
+            import: '__cssprop',
+            module: '@emotion/react',
+          },
+        ],
+        [
+          '@babel/plugin-transform-react-jsx',
+          { pragma: '__cssprop' },
+          'twin.macro',
+        ],
+      ]
+    }
+  })],
   server: {
     port: 3000,
     proxy: {
