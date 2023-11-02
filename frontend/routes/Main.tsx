c87b33a56c5dfee755bba7a9bb8de6415d0b0f27
kemper0110 <danil2003.2043@gmail.com>
2023-11-02 17:01:13 +0300

    main menu links

--- frontend/routes/Main.tsx
+++ frontend/routes/Main.tsx
@@ -1,15 +1,26 @@
 import {Link} from "react-router-dom";
+import tw from "twin.macro";
+import styled from "@emotion/styled";
+import {css} from "@emotion/react";
 
 export const Main = () => {
     return (
         <div className={'text-white bg-stone-800 w-full min-h-screen pt-[50px] flex place-items-center'}>
             <div className={'mx-auto w-[558px] flex flex-col items-center gap-6'}>
-                <Link to={'/games'}>
-                    Games
-                </Link>
-                <Link to={'/create-game'}>
-                </Link>
+                <MainMenuLink to={'/games'}>
+                    Список игр
+                </MainMenuLink>
+                <MainMenuLink className={''} to={'/create-game'}>
+                    Создать игру
+                </MainMenuLink>
             </div>
         </div>
     )
 }
+
+const MainMenuLink = styled(Link)(() => ([
+    tw`text-5xl font-bold`,
+    css`&:hover {
+      text-shadow: 0px 6px 10px rgba(255, 255, 255, 0.7)
+    }`
+]))
