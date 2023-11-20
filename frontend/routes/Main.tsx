1a0f182527d422a6f5bfa4ed8c21573ef8da840f
kemper0110 <danil2003.2043@gmail.com>
2023-11-20 22:35:24 +0300

    rework bg code

--- frontend/routes/Main.tsx
+++ frontend/routes/Main.tsx
@@ -7,10 +7,9 @@ import video from "@/assets/video_lowBitRate_AV1_60fps.mp4";
 export const Main = () => {
     return (
         <div tw={'w-full min-h-screen relative'}>
-            <div className={'absolute h-screen w-full'}>
-                <video src={video} playsInline autoPlay muted loop className={'object-cover h-full w-full'}/>
-            </div>
-            <div tw={'absolute text-white flex place-items-center h-screen w-full'}>
+            <video src={video} preload={'auto'} playsInline autoPlay muted loop
+                   className={'absolute inset-0 object-cover h-full w-full'}/>
+            <div tw={'absolute inset-0 text-white flex place-items-center h-screen w-full'}>
                 <div tw={'mx-auto w-[558px] flex flex-col items-center gap-6'}>
                     <MainMenuLink to={'/games'}>
                         Список игр
