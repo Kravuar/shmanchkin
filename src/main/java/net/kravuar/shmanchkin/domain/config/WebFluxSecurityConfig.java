8b0d5a391777d5510a1134b5402593eae0426115
kravuar <steam_webmoney@mail.ru>
2023-11-25 11:00:58 +0300

    key loading fix, readme

--- src/main/java/net/kravuar/shmanchkin/domain/config/WebFluxSecurityConfig.java
+++ src/main/java/net/kravuar/shmanchkin/domain/config/WebFluxSecurityConfig.java
@@ -11,9 +11,10 @@ import net.kravuar.shmanchkin.domain.security.PrincipalExtractor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
 
-import java.nio.file.Files;
+import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -26,29 +27,30 @@ import java.util.UUID;
 public class WebFluxSecurityConfig {
     private final GameService gameService;
 
-    @Value("${web.jwt.publicKeyFile}")
-    private String publicKeyPath;
+    @Value("classpath:${web.jwt.publicKeyResource}")
+    private Resource publicKeyResource;
 
-    @Value("${web.jwt.privateKeyFile}")
-    private String privateKeyPath;
+    @Value("classpath:${web.jwt.privateKeyResource}")
+    private Resource privateKeyResource;
 
     @Bean
     @SneakyThrows
-    public Algorithm algorithm(ResourceLoader loader) {
-        var publicKeyResource = loader.getResource(publicKeyPath);
-        var privateKeyResource = loader.getResource(privateKeyPath);
+    public Algorithm algorithm() {
+        try (InputStream publicKeyStream = publicKeyResource.getInputStream();
+             InputStream privateKeyStream = privateKeyResource.getInputStream()) {
 
-        byte[] publicKeyBytes = Files.readAllBytes(publicKeyResource.getFile().toPath());
-        byte[] privateKeyBytes = Files.readAllBytes(privateKeyResource.getFile().toPath());
+            byte[] publicKeyBytes = StreamUtils.copyToByteArray(publicKeyStream);
+            byte[] privateKeyBytes = StreamUtils.copyToByteArray(privateKeyStream);
 
-        var keyFactory = KeyFactory.getInstance("RSA");
-        var publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-        var privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
 
-        RSAPublicKey publicKey = (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory.generatePrivate(privateKeySpec);
+            RSAPublicKey publicKey = (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory.generatePrivate(privateKeySpec);
 
-        return Algorithm.RSA256(publicKey, privateKey);
+            return Algorithm.RSA256(publicKey, privateKey);
+        }
     }
 
     @Bean
